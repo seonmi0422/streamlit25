615289cd300aa821486086333cf34328505a43a5
KY7437 <eugene7437@gmail.com>
2025-05-14 14:34:06 +0900

    Update About.py

--- pages/About.py
+++ pages/About.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.write("This app is under construction.")
 
-url="https://github.com/KY7437/streamlit25/raw/main/images/image01.jpg"
-st.image(url, caption="Image link", width=300)  # width in pixels
+url="https://github.com/KY7437/streamlit25/raw/main/images/Kouchi.jpg"
+st.image(url, caption="Image link", width=1000)  # width in pixels
 
