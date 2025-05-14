0a4b87908ae4d9e649d9d42bf83b0e584c1b1dca
seonmi0422 <joseonmi873@gmail.com>
2025-05-14 21:28:54 +0900

    Update About.py

--- pages/About.py
+++ pages/About.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.write("This app is under construction.")
 
-url="https://github.com/MK316/streamlit25/raw/main/images/image00.png"
+url="https://chatgpt.com/s/m_68248c1c82d48191b506be2a8dac7fc8"
 st.image(url, caption="Image link", width=300)  # width in pixels
 
