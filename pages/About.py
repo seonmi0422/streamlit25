7e59b642ad1133c5cfe245b8f68b865ffcc53a6d
KY7437 <eugene7437@gmail.com>
2025-05-14 14:34:20 +0900

    Update About.py

--- pages/About.py
+++ pages/About.py
@@ -3,5 +3,5 @@ import streamlit as st
 st.write("This app is under construction.")
 
 url="https://github.com/KY7437/streamlit25/raw/main/images/Kouchi.jpg"
-st.image(url, caption="Image link", width=1000)  # width in pixels
+st.image(url, caption="Image link", width=700)  # width in pixels
 
