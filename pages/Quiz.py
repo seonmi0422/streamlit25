38201b99897564c14baf2d3a9f0d6398d70e10a8
KY7437 <eugene7437@gmail.com>
2025-05-16 12:49:11 +0900

    Update Quiz.py

--- pages/Quiz.py
+++ pages/Quiz.py
@@ -1,1 +1,18 @@
+import streamlit as st
+import pandas as pd
+from gtts import gTTS
+from io import BytesIO
+import random
 
+st.write("Quiz")
+
+tab1, tab2, tab3, tab4 = st.tabs(["❄️ Vocabulary", "❄️ 2. Grammar", "❄️ 3. Passage"])
+
+with tab1:
+  dd
+
+with tab2:
+  dd
+
+with tab3:
+  dd 
